9ce696cd06e55d5b9ca0db8f01413c2f5a46653e
dyahzhafira <dyahzhafira.w@gmail.com>
2026-01-31 00:38:13 +0700

    feat: add values data

---
+++ src/constants/values.ts
@@ -0,0 +1,41 @@
+import {
+  Zap,
+  Globe,
+  Users,
+} from "lucide-react";
+
+export const VALUES_DATA = [
+  {
+    id: "friendship",
+    title: "Friendship",
+    icon: Users,
+    color: "bg-rose-500",
+    bgLight: "bg-rose-50",
+    text: "text-rose-600",
+    description: "Having a strong drive to be excellent leaders who connect people to accomplish shared goals.",
+    points: ["TIBA TIBA", "Grand Team Building", "Training Division"],
+    img:"vision/friendship.jpeg"
+  },
+  {
+    id: "leadership",
+    title: "Leadership",
+    icon: Zap,
+    color: "bg-blue-600",
+    bgLight: "bg-blue-50",
+    text: "text-blue-600",
+    description: "A profound organization in leadership, serving as a bridge between the professional world and highly motivated students.",
+    points: ["SxC Impact", "SxCamp"],
+    img: "vision/leadership.jpeg"
+  },
+  {
+    id: "partnership",
+    title: "Partnership",
+    icon: Globe,
+    color: "bg-emerald-500",
+    bgLight: "bg-emerald-50",
+    text: "text-emerald-600",
+    description: "Encouraging partnership and collaboration with many parties.",
+    points: ["SharingXChapter", "SxCamp"],
+    img: "vision/partnership.jpeg"
+  }
+];
